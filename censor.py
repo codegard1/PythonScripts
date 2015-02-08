b2a233f57163c4f41a7be09ed508c6ebc42b0de5
Chris Odegard <c.odegard@gmail.com>
2015-02-08 16:12:04 -0500

    fixed censor.py
    Figured out how to get censor.py to do what it's supposed to do, however
    it does not seem very efficient.

--- censor.py
+++ censor.py
@@ -16,17 +16,26 @@
 def censor(text, word):
     print(text, '/', word) #debugging
     i = 0 
-    c = 0
+    n = 0
     wordlist = text.split(' ')
+    
     for i in range(len(wordlist)): #take each token from text in turn
-        print(i, wordlist[i]) #debugging
+        #print(i, wordlist[i]) #debugging
+
         if wordlist[i].lower() == word.lower(): #if w = word
-            for c in wordlist[i]:
-                c = '*'
-            print(wordlist[i])
-    return ' '.join(wordlist)
+            replacement = list(wordlist[i])
+            
+            for n in range(len(replacement)):
+                replacement[n] = '*'
+                n += 1
+                
+            wordlist[i] = ''.join(replacement) #rejoin the replacement list chars
+    wordlist = ' '.join(wordlist) # recreate the sentence str       
+
+    return wordlist #return the str
 
 
-t = 'This shit is whack'
-w = 'shit'
+t = 'Once there was a man whose men were men and whose man was a men'
+w = 'man'
+
 print(censor(t, w))
